f0741bc5208913cb59518eb9c02042091b554bbe
shrishailamaganoor-creator <shrishailamaganoor@gmail.com>
2026-06-27 23:32:37 +0530

    Add files via upload

---
+++ shopping_listproject 2.py
@@ -0,0 +1,19 @@
+print("="*62)
+print( """Make your shopping list""" )
+print("="*62)
+shopping_list = [ ]
+while True:
+    add = input("tell me if u want to add an item to your list y/n:").lower().strip
+    if add in ["yes","y"]:
+             item = input("Enter the item:").strip()
+             shopping_list.append(item)
+    elif add in ["no","n"]:
+             break
+    else:
+        print("please enter yes or no")
+    
+print('Your shopping list would be')
+count= 1
+for item in shopping_list:
+    print(count,".",item)
+    count+=1
